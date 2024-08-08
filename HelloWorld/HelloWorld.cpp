20b41a4d71318f3a77d36e6bfe7d8b07f761cb62
dmigwi <migwindungu0@gmail.com>
2024-08-08 23:38:39 +0200

    Add printstandard solution

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <limits>
 
-int main() 
-{
-    std::cout << "Hello World, Jambo!";
-    return 0;
+int main() {
+  std::cin.clear();  // reset any error flags
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(),
+                  '\n');  // ignore any characters in the input buffer until we
+                          // find a newline
+  std::cin.get();  // get one more char from the user (waits for user to press
+                   // enter)
+  std::cout << "Hello World, Jambo!" << std::endl;
+  return 0;
 }
